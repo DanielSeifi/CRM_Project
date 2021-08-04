e30876f5f7882a6319b7646fc41ac79c06dc7486
Daniel Seifi <seyfidanial@gmail.com>
2021-08-04 20:33:04 +0430

    update product and organization and create quote

--- Quote/models.py
+++ Quote/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models import Sum, F, Case, When
 from django_jalali.db import models as jdate
 from django.utils.translation import ugettext_lazy as _
 # Create your models here.
@@ -20,6 +21,43 @@ class Quote(models.Model):
 
     def __str__(self):
         return f'{self.organization}'
+
+    def get_total_quantity(self):
+        return self.quoteitem_set.all().aggregate(Sum('quantity')).get('quantity__sum', 0)
+
+    def get_total_base_price(self):
+        return self.quoteitem_set.all().annotate(total_base_price=F('quantity') * F('price')) \
+            .aggregate(Sum('total_base_price'))['total_base_price__sum']
+
+    def get_quote_discount(self):
+        return self.quoteitem_set.all().annotate(
+            total_base_price=F('quantity') * F('price')).annotate(
+            total_discount=(F('discount') * F('total_base_price') / 100)) \
+            .aggregate(Sum('total_discount'))['total_discount__sum']
+
+    def get_quote_tax(self, tax=tax):
+        return self.quoteitem_set.all().annotate(
+            total_base_price=F('quantity') * F('price')).annotate(
+            total_price=F('total_base_price') - (F('discount') * F('total_base_price') / 100)).annotate(
+            total_tax=Case(
+                When(product__tax_status=True, then=(F('total_price') * tax / 100)),
+                When(product__tax_status=False, then=0),
+                output_field=models.PositiveIntegerField()
+            )
+        ).aggregate(Sum('total_tax'))['total_tax__sum']
+
+    def get_total_price(self, tax=tax):
+        return self.quoteitem_set.all().annotate(
+            total_base_price=F('quantity') * F('price')).annotate(
+            total_price=F('total_base_price') - (F('discount') * F('total_base_price') / 100)).annotate(
+            total_price=Case(
+                When(product__tax_status=True, then=F('total_price') + (F('total_price') * tax / 100)),
+                When(product__tax_status=False, then=F('total_price')),
+                output_field=models.PositiveIntegerField()
+            )
+        ).aggregate(Sum('total_price'))['total_price__sum']
+
+
 
 class QuoteItem(models.Model):
     quote = models.ForeignKey(Quote, on_delete=models.CASCADE, verbose_name=_('پیش فاکتور'))
